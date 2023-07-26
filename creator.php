3a2711febce3a3a3f8fb3fce079046262c93f999
DavidFFDAW <davidferflo2@gmail.com>
2023-07-27 00:54:09 +0200

    fix: creator classes names

--- creator.php
+++ creator.php
@@ -3,18 +3,20 @@
 define('B', PHP_EOL);
 define('D', DIRECTORY_SEPARATOR);
 
-define('API_DIR', dirname(__FILE__).D.'api'.D);
+define('API_DIR', dirname(__FILE__) . D . 'api' . D);
 define('VALID_TYPES', array('controller', 'middleware', 'class'));
 
-function help() {
-    echo "--------------------------------------------------------------------".B;
-    echo "    Usage of the creator: ".B;
-    echo "    - php creator.php create <type>* <name>".B;
-    echo "       * where <type> can be one of [".implode(", ", VALID_TYPES)."]".B;
+function help()
+{
+    echo "--------------------------------------------------------------------" . B;
+    echo "    Usage of the creator: " . B;
+    echo "    - php creator.php create <type>* <name>" . B;
+    echo "       * where <type> can be one of [" . implode(", ", VALID_TYPES) . "]" . B;
 }
 
-function create($type, $name) {
-    if (empty($type) || empty($name)) throw new Error("Some parameters were not inputted");   
+function create($type, $name)
+{
+    if (empty($type) || empty($name)) throw new Error("Some parameters were not inputted");
     if (!in_array($type, VALID_TYPES)) throw new Error('Not a valid type');
 
     $lookup = array(
@@ -24,7 +26,7 @@ function create($type, $name) {
     );
 
     $plural = $lookup[$type];
-    $fileName = ucfirst($name) . ucfirst(($type == 'class') ? 'model' : $type);
+    $fileName = ucfirst($name) . (($type === 'class') ? '' : (ucfirst($type)));
 
     $extendsOrImplements = array(
         'controller' => 'extends BaseController',
@@ -33,7 +35,7 @@ function create($type, $name) {
     );
     $extendsOrImplementsText = $extendsOrImplements[$type];
 
-    file_put_contents(API_DIR.$plural.D.$fileName.".php", "<?php
+    file_put_contents(API_DIR . $plural . D . $fileName . ".php", "<?php
     
 class $fileName $extendsOrImplementsText {}");
 }
@@ -52,9 +54,6 @@ try {
     if ($operation === 'create') {
         create($type, $name);
     }
-
 } catch (Error $er) {
-    echo $er->getMessage().B;
+    echo $er->getMessage() . B;
 }
-
-
