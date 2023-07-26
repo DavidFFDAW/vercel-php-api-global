d80beded20f713afd3389f146482f55a5e59a5a3
DavidFFDAW <davidferflo2@gmail.com>
2023-07-26 12:55:56 +0200

    feat(RoutesDispatcher): regex router control for parameters

--- api/routes/RoutesDispatcher.php
+++ api/routes/RoutesDispatcher.php
@@ -2,12 +2,61 @@
 
 class RoutesDispatcher
 {
-      private function searchForRoute($routes, $endpoint)
+      private function convertRoute($routeObject, $endpoint, Request $request) {
+            $params = [];
+            $paramKey = [];
+            $reqUri = $endpoint;
+
+            $route = $routeObject['endpoint'];
+            $route = preg_replace("/(^\/)|(\/$)/","", $route);
+            preg_match_all("/(?<={).+?(?=})/", $route, $paramMatches);
+            $hasParams = !empty($paramMatches[0]);
+
+            if ($hasParams) {
+                  foreach($paramMatches[0] as $key){
+                        $paramKey[] = $key;
+                  }
+                  $uri = explode("/", $route);
+                  $indexNum = []; 
+
+                  foreach($uri as $index => $param){
+                        if(preg_match("/{.*}/", $param)){
+                              $indexNum[] = $index;
+                        }
+                  }
+                  
+                  $reqUri = explode("/", $endpoint);
+                  
+                  foreach($indexNum as $key => $index){
+                        if(!empty($reqUri[$index])){
+                              $params[$paramKey[$key]] = $reqUri[$index];
+                              $reqUri[$index] = "{.*}";
+                        }
+                  }
+                  $reqUri = implode("/",$reqUri);
+                  
+            }
+            
+            
+            $reqUri = str_replace("/", '\\/', $reqUri);
+            $match = preg_match("/$reqUri/", $route);
+            
+            if($match) {
+                  $request->setParameters($params);
+            }
+
+            return $match;
+      }
+
+      private function searchForRoute($routes, $endpoint, Request $request)
       {
             $found = array();
-
+            $endpoint = preg_replace("/(^\/)|(\/$)/", "", $endpoint);
+            
             foreach ($routes as $route) {
-                  if ($route['endpoint'] === $endpoint) {
+                  $doRouteMatch = $this->convertRoute($route, $endpoint, $request);
+
+                  if ($doRouteMatch) {
                         $found = $route;
                         break;
                   }
@@ -19,11 +68,11 @@ class RoutesDispatcher
       public function dispatch(array $routes, Request $request)
       {
             $methodRoutes = $routes[$request->method];
-            $uri = $request->requestUri;
+            $uri = $request->getURI();
 
             if (empty($methodRoutes) || empty($uri)) throw new ApiException("There are no routes for this method and endpoint");
 
-            $foundRoute = $this->searchForRoute($methodRoutes, $uri);
+            $foundRoute = $this->searchForRoute($methodRoutes, $uri, $request);
 
             if (empty($foundRoute)) throw new ApiException("Custom Route $uri not found");
 
@@ -37,6 +86,6 @@ class RoutesDispatcher
             $controller = new $Controller();
             if (!method_exists($controller, $controllerMethod)) throw new Error("Controller $controller: $controllerMethod does not exist in this controller");
 
-            return $controller->$controllerMethod($request);
+            die($controller->$controllerMethod($request));
       }
 }
