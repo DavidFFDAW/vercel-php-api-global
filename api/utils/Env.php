fc6b763c6438e80726c63f34de27d089ff82af78
DavidFFDAW <davidferflo2@gmail.com>
2023-07-27 14:26:51 +0200

    chore: .env read if dev

--- api/utils/Env.php
+++ api/utils/Env.php
@@ -2,8 +2,29 @@
 
 class Env
 {
+      private static function getEnvKeys() {
+            $path = DIR.".env";
+            $finalAssociativeArray = array();
+
+            if (!file_exists($path)) return [];
+
+            $environ = file_get_contents($path);
+
+            foreach (explode("\n", $environ) as $var) {
+                  $exploded = explode("=", $var);
+                  $finalAssociativeArray[trim($exploded[0])] = trim($exploded[1]);
+            }
+
+            return $finalAssociativeArray;
+      }
+      
+
       public static function getEnvVars()
       {
+            if (DEV) {
+                  return self::getEnvKeys();
+            }
+
             return array(
                   'DB_HOST' => getenv('DB_HOST'),
                   'DB_USER' => getenv('DB_USER'),
