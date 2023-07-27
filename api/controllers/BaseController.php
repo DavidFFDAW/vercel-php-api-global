46c1b863b9c9fa65b11a979098ba9055534d510a
DavidFFDAW <davidferflo2@gmail.com>
2023-07-27 11:04:23 +0200

    chore(BaseController): getRequestID method

--- api/controllers/BaseController.php
+++ api/controllers/BaseController.php
@@ -2,6 +2,17 @@
 
 abstract class BaseController
 {
+      protected function getTheRequestID(Request $req) {
+            $id = isset($req->params->id) ? $req->params->id : '';
+            if (!$this->isChecker($id)) throw new ApiException("No ID indicated to delete");
+
+            return $id;
+      }
+
+      protected function isChecker($condition) {
+            return isset($condition) && !empty($condition);
+      }
+
       protected function response($data, string $key = '', int $code = 200)
       {
             $realKey = empty($key) ? 'data' : $key;
