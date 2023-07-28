0f4f92d419831242ad8c7a7c7206323249a614c0
DavidFFDAW <davidferflo2@gmail.com>
2023-07-28 14:25:21 +0200

    feat: scaping database functions

--- api/classes/BaseModel.php
+++ api/classes/BaseModel.php
@@ -4,55 +4,65 @@ abstract class BaseModel extends QueryBuilder implements ModelInterface
 {
       public static $tableS = '';
 
-      private static function filters(array $filters) {
+      private static function filters(array $filters)
+      {
             $where = false;
-            $sql = "SELECT * FROM `".static::$tableS."` ";
+            $db = Db::getInstance();
+            $sql = "SELECT * FROM `" . static::$tableS . "` ";
 
             foreach ($filters as $filter) {
                   $w = ($where ? "AND WHERE " : "WHERE ");
-                  $key = $filter[0];
-                  $operator = $filter[1];
-                  $value = (strtoupper($operator) == 'LIKE') ? '%'.$filter[2].'%' : $filter[2];
-                  
-                  $sql .= $w."`$key` ".$operator." ".$value;
+                  $key = $db->scape($filter[0]);
+                  $operator = $db->scape($filter[1]);
+                  $isLike = (strtoupper($operator) == 'LIKE');
+                  $value = $db->scape($isLike ? '%' . $filter[2] . '%' : $filter[2]);
+                  $finalValue = gettype($value) === 'string' ? "'" . $value . "'" : $value;
+
+                  $sql .= $w . "`$key` " . $operator . " " . ($finalValue);
             }
+
             return $sql;
       }
 
-      public static function find($id) {
-            $sql = "SELECT * FROM `".static::$tableS."` WHERE `id` = $id";
-            return Db::getInstance()->getRow($sql);
+      public static function find($id)
+      {
+            $db = Db::getInstance();
+            $sql = "SELECT * FROM `" . static::$tableS . "` WHERE `id` = " . $db->scape($id);
+            return $db->getRow($sql);
       }
 
-      public static function findAll() {
-            $sql = "SELECT * FROM `".static::$tableS."`";
+      public static function findAll()
+      {
+            $sql = "SELECT * FROM `" . static::$tableS . "`";
             return Db::getInstance()->query($sql);
       }
-      
-      public static function findBy(array $filters) {
+
+      public static function findBy(array $filters)
+      {
             $sql = self::filters($filters);
             return Db::getInstance()->query($sql);
       }
-      
-      public static function findOneBy(array $filters) {
+
+      public static function findOneBy(array $filters)
+      {
             $sql = self::filters($filters);
             return Db::getInstance()->getRow($sql);
       }
-      
-      public static function create($data) {
-          
-      }
-      
-      public static function update($data) {
-          
-      }
-      
-      public static function delete($id) {
-          
+
+      public static function create($data)
+      {
       }
 
-      public static function query(string $sql) {
+      public static function update($data)
+      {
+      }
+
+      public static function delete($id)
+      {
+      }
+
+      public static function query(string $sql)
+      {
             return Db::getInstance()->query($sql);
       }
-      
 }
--- api/database/Db.php
+++ api/database/Db.php
@@ -1,10 +1,12 @@
 <?php
 
-class Db {
+class Db
+{
     private $conn;
     private static $instance = null;
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (self::$instance == null) {
             self::$instance = new Db();
         }
@@ -23,11 +25,23 @@ class Db {
         return $this->conn;
     }
 
-    public function query($sq) {
+    public static function scapeString(string $str)
+    {
+        return Db::getInstance()->getConnection()->real_escape_string($str);
+    }
+
+    public function scape(string $str)
+    {
+        return $this->conn->real_escape_string($str);
+    }
+
+
+    public function query($sq)
+    {
         $finalResult = array();
         $result = $this->conn->query($sq);
 
-        
+
         if ($result->num_rows > 0) {
             foreach ($result as $res) {
                 $finalResult[] = $res;
