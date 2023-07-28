4e915c783b445dc495974a001a3387c782673699
DavidFFDAW <davidferflo2@gmail.com>
2023-07-28 14:26:12 +0200

    chore: Autoload and Headers abstractions and variables

---
+++ api/utils/Header.php
@@ -0,0 +1,18 @@
+<?php
+
+class Header
+{
+    public static function setHeaders()
+    {
+        header('Content-type: application/json');
+        header('Accept: *');
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
+    }
+}
