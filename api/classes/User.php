88c4c1189ffbc42de2bb3db2abefd3805437796a
DavidFFDAW <davidferflo2@gmail.com>
2023-08-01 00:32:11 +0200

    fix: user token search

--- api/classes/User.php
+++ api/classes/User.php
@@ -7,7 +7,7 @@ class User extends BaseModel
 
     public static function getUserByToken(string $token)
     {
-        $user = self::findOneBy([array('api_token', '=', "'$token'")]);
+        $user = self::findOneBy([array('api_token', '=', "$token")]);
 
         if (empty($user)) throw new ApiException("No user found with this token");
 
