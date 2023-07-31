86a013ea9db1fe4427d3cddde7299cb713cbc6db
DavidFFDAW <davidferflo2@gmail.com>
2023-08-01 00:31:19 +0200

    chore: Content json header order

--- api/utils/Header.php
+++ api/utils/Header.php
@@ -4,12 +4,12 @@ class Header
 {
     public static function setHeaders()
     {
-        header('Content-type: application/json');
         header('Accept: *');
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+        header('Content-type: application/json');
         $method = $_SERVER['REQUEST_METHOD'];
         if ($method == "OPTIONS") {
             die();
