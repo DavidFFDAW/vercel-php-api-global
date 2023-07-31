5a1427397b393cb0f5bde1dc5a16530e21934ed8
DavidFFDAW <davidferflo2@gmail.com>
2023-08-01 00:32:43 +0200

    wip: update and insert

--- api/classes/BaseModel.php
+++ api/classes/BaseModel.php
@@ -3,6 +3,8 @@
 abstract class BaseModel extends QueryBuilder implements ModelInterface
 {
       public static $tableS = '';
+      public $_table = '';
+      protected $attributes = array();
 
       private static function filters(array $filters)
       {
@@ -22,6 +24,37 @@ abstract class BaseModel extends QueryBuilder implements ModelInterface
             }
 
             return $sql;
+      }
+
+      public function getAttributesParsed($body, $key = 'db_name')
+      {
+            $final = array();
+            $body = (array) $body;
+
+            foreach ($this->attributes as $attribute) {
+                  $apiName = $attribute->getAPIName();
+
+                  if (isset($body[$apiName]) && !empty($body[$apiName])) {
+                        $final[$apiName] = $body[$apiName];
+                  }
+            }
+
+            return $final;
+      }
+
+      public function getRequiredAttributesParsed($body, $key = 'api_name')
+      {
+            $final = array();
+            $body = (array) $body;
+
+            foreach ($this->attributes as $attribute) {
+                  if ($attribute->isRequired()) {
+                        $apiName = $attribute->getAPIName();
+                        $final[$apiName] = isset($body[$apiName]) ? $body[$apiName] : '';
+                  }
+            }
+
+            return $final;
       }
 
       public static function find($id)
@@ -49,12 +82,68 @@ abstract class BaseModel extends QueryBuilder implements ModelInterface
             return Db::getInstance()->getRow($sql);
       }
 
-      public static function create($data)
+      public function create($data)
+      {
+      }
+      private function getPreparedDatas($datas)
+      {
+            $str = [];
+            $types = [];
+            $values = [];
+            $keys = [];
+
+            foreach ($datas as $key => $value) {
+                  if (!isset($this->attributes[$key])) continue;
+
+                  $att = $this->attributes[$key];
+
+                  if (!$att->isID()) {
+                        $str[] = "`" . $att->getDbName() . "` = ?";
+                  }
+                  $types[] = $att->getType();
+                  $values[] = $value;
+                  $keys[] = $att->getDbName();
+            }
+
+            return array(
+                  'string' => implode(", ", $str),
+                  'types' => $types = implode("", $types),
+                  'values' => $values,
+                  'keys' => $keys,
+            );
+      }
+
+      private function insert($datas)
       {
       }
 
-      public static function update($data)
+      private function update($datas)
       {
+            $prepared = $this->getPreparedDatas($datas);
+            $sql = "UPDATE `" . $this->_table . "` SET " . $prepared['string'] . " WHERE id = ?";
+
+            // bind params with keys and types
+            $stmt = Db::getInstance()->prepare($sql);
+            // $stmt->bind_param($types, ...$values);
+
+            Debug::ddAPI(array(
+                  'SQL' => $sql,
+                  'stmt' => $stmt,
+                  'types' => $prepared['types'],
+                  'values' => $prepared['values'],
+                  'keys' => $prepared['keys'],
+                  'error' => error_get_last(),
+            ));
+
+            // foreach ()
+      }
+
+      public function upsert($data)
+      {
+            if (isset($data['id']) && !empty($data['id'])) {
+                  return $this->update($data);
+            }
+            return $this->insert($data);
       }
 
       public static function delete($id)
