865cef43aa159ade16c1ff2dbbe752e2a211ed73
DavidFFDAW <davidferflo2@gmail.com>
2023-08-01 09:42:14 +0200

    fix: error email User search

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends BaseController
 
         if (empty($body->email)) throw new ApiException("Email was not present in request");
 
-        $foundUser = User::findOneBy([array('email', '=', "'" . $body->email . "'")]);
+        $foundUser = User::findOneBy([array('email', '=', $body->email)]);
         if (!$foundUser || empty($foundUser)) throw new ApiException("User was not found with this email");
         $isCorrect = PasswordService::verify($body->password, $foundUser['password']);
 
