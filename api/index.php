bd6ef8a072b219f107a53724072ebb3e846220f7
DavidFFDAW <davidferflo2@gmail.com>
2023-08-04 18:38:05 +0200

    fix: debug spl autoload on index

--- api/index.php
+++ api/index.php
@@ -3,9 +3,28 @@ error_reporting(E_ALL & ~E_NOTICE);
 define('DIR', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('API', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
-require_once(API . "Autoload.php");
+// require_once(API . "Autoload.php");
 
-Autoload::register();
+// Autoload::register();
+
+spl_autoload_register(function ($class) {
+      $controllerDirectory = API . "controllers" . DIRECTORY_SEPARATOR . "$class.php";
+      $classDirectory = API . "classes" . DIRECTORY_SEPARATOR . "$class.php";
+      $routesDirectory = API . "routes" . DIRECTORY_SEPARATOR . "$class.php";
+      $utilsDirectory = API . "utils" . DIRECTORY_SEPARATOR . "$class.php";
+      $servicesDirectory = API . "services" . DIRECTORY_SEPARATOR . "$class.php";
+      $databaseDirectory = API . "database" . DIRECTORY_SEPARATOR . "$class.php";
+      $middlewareDirectory = API . "middlewares" . DIRECTORY_SEPARATOR . "$class.php";
+
+      if (file_exists($controllerDirectory)) require_once($controllerDirectory);
+      if (file_exists($classDirectory)) require_once($classDirectory);
+      if (file_exists($routesDirectory)) require_once($routesDirectory);
+      if (file_exists($utilsDirectory)) require_once($utilsDirectory);
+      if (file_exists($servicesDirectory)) require_once($servicesDirectory);
+      if (file_exists($databaseDirectory)) require_once($databaseDirectory);
+      if (file_exists($middlewareDirectory)) require_once($middlewareDirectory);
+});
+
 Header::setHeaders();
 define('DEV', $_SERVER['HTTP_HOST'] == 'localhost:8555');
 
